9dfd26f1d943fc66eff4f6b670651e59d2f41803
Sithila69 <kms123sithila@gmail.com>
2025-08-10 01:30:11 +0530

    added register customer controller

--- backend/src/routes/listings-routes.ts
+++ backend/src/routes/listings-routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 
 import {
-  debugListings,
   getAllListings,
   getListingDetails,
   getListingsByCategory,
@@ -15,7 +14,6 @@ const router = Router();
 router.get("/all", getAllListings);
 router.get("/", getAllListings);
 router.get("/new-arrivals", getNewArrivals); // Get new arrivals with date filtering
-router.get("/debug-listings", debugListings);
 router.get("/categories", getCategories); // Get all categories
 router.get("/category/:categorySlug", getListingsByCategory); // Get listings by category slug
 router.get("/get-details/:idOrSlug", getListingDetails);
